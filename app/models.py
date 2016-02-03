e926162ea227a842c2c0890dc08328c89bdb34ad
tripathiraghvendra604 <tripathiraghvendra604@gmail.com>
2016-02-04 05:03:51 +0530

    made the database

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db
+
+class AnyDo(db.Model):
+    id = db.Column(db.Integer(),primary_key=True)
+    body = db.Column(db.String())
+    user = db.Column(db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<DO %s>' %self.body
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(200))
+    password = db.Column(db.String(20))
+    email = db.Column(db.String(200))
+
+    def __repr__(self):
+        return '<User %s>' %self.name
